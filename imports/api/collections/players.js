414774691b67e26ff29d04f9042b0d7b5ed06416
Brendan W. Weimer <brendanweimer@gmail.com>
2018-03-12 15:10:11 -0500

    created player collection and scaffold for rendering individual players and the whole list of players

---
+++ imports/api/collections/players.js
@@ -0,0 +1,55 @@
+import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
+import { DDPRateLimiter } from 'meteor/ddp-rate-limiter';
+
+Meteor.methods({
+  //---------------------------------------------------------------------------
+  'player.remove': function (player) {
+    check(player._id, String);
+
+    return player.remove(player._id);
+  },
+  //---------------------------------------------------------------------------
+  'player.update': function (player) {
+    check(player._id, String);
+    check(player.number, Number);
+    check(player.name, String);
+    check(player.position, String);
+
+    return player.update(player._id,{
+      $set: {
+        number: player.number,
+        name: player.name,
+        position: player.position,
+      },
+    });
+  },
+  //---------------------------------------------------------------------------
+  'player.insert': function (player) {
+    check(player.number, Number);
+    check(player.name, String);
+    check(player.position, String);
+
+    return Player.insert({
+      number: player.number,
+      name: player.name,
+      position: player.position,
+    });
+  },
+});
+
+export const Player = new Mongo.Collection('player');
+//Get list of all method names
+const PLAYER_METHODS = ['player.remove', 'player.update', 'player.insert'];
+
+if (Meteor.isServer) {
+  //only allow 5 operation per connection per second
+  DDPRateLimiter.addRule({
+    name(name) {
+      retunr_.contains(PLAYER_METHODS, name);
+    },
+    connectionId(){return true;};
+  }, 5, 1000);
+}
